997c058ee47e23f009acfbc02e38b0032bc280e0
KIMGANGYEON <rkddus3177@naver.com>
2024-01-29 13:47:52 +0900

    good

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.portfolio-item1')[0].addEventListener('mouseenter', function(){
+    document.querySelectorAll('.portfolio-item1')[0].classList.add('scale')
+})
+
+document.querySelectorAll('.portfolio-item1')[0].addEventListener('mouseleave', function(){
+    document.querySelectorAll('.portfolio-item1')[0].classList.remove('scale')
+})
